cf407bb1dd91de18597283080250c3ffbbf99b2c
Nicolas Ribeiro Carvalho <nicolascarvalho856@gmail.com>
2026-01-20 19:32:58 -0300

    arrumando as pastas e caminho do arquivo xlms

--- script/projeto_1.py
+++ script/projeto_1.py
@@ -1,8 +1,16 @@
 #importanto bibliotecas e base de dados
 import pandas as pd
+from pathlib import Path
 
-tabela_vendas = pd.read_excel("C:\\Users\\nicol\\Downloads\\Vendas+-+Base+de+Dados.xlsx")
-print(tabela_vendas)
+# caminho da pasta onde está este arquivo .py
+BASE_DIR = Path(__file__).resolve().parent
+
+# sobe uma pasta e acessa o Excel
+caminho_excel = BASE_DIR.parent / "Vendas+-+Base+de+Dados.xlsx"
+
+tabela_vendas = pd.read_excel(caminho_excel)
+
+print(tabela_vendas.head())
 
 #Calculando produto mais vendido (quantidade)
 
